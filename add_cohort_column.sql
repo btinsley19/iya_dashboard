9bec4de6420ce9ef53e66f6389063a3fa091cefd
BT <btinsley@usc.edu>
2025-09-29 22:23:21 -0700

    Delete add_cohort_column.sql

--- add_cohort_column.sql
+++
@@ -1,6 +0,0 @@
--- Add cohort column to profiles table if it doesn't exist
-ALTER TABLE profiles 
-ADD COLUMN IF NOT EXISTS cohort TEXT;
-
--- Create index for cohort field
-CREATE INDEX IF NOT EXISTS idx_profiles_cohort ON profiles(cohort);
